e03b308d1fc7e94b2fe8028708afc8c59c4a69d6
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-08-07 14:11:44 +0200

    starting frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header.tsx';
+import Main from './Components/Main.tsx';
+import Footer from './Components/Footer.tsx';
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Header.tsx
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+        <h1>Hi</h1>
+    )
+}
+
+export default Header
