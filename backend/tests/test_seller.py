c375b23d44c9d4e5a230937ce17bae07bf138f56
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-08-24 17:56:37 +0200

    learning github actions and testing

---
+++ backend/tests/test_seller.py
@@ -0,0 +1,45 @@
+from main import app
+from fastapi.testclient import TestClient
+from response.sellerResponse import SellerReponse
+
+client = TestClient(app)
+
+def test_create_seller():
+    response = client.post("/seller/add_car")
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "hi",
+        "brand_id": 1,
+        "model_id": 1,
+        "type_id": 1,
+        "fuel_id": 1,
+        "tachometer": 1,
+        "made_at": 1,
+        "description": "hi",
+        "car_body_id": 1,
+        "gearbox_id": 1,
+        "power": 1,
+        "place_of_sale": "hi",
+        "country_of_car": 1,
+        "history": "hi",
+        "price": 1,
+        "min_price": 1
+    }
+
+def test_read_seller():
+    response = client.get("/seller/1")
+    assert response.status_code == 200
+
+def test_update_test():
+    response = client.put("/seller/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "sold": True,
+        "price": 1,
+        "min_price": 1
+    }
+
+def test_delete_seller():
+    response = client.delete("/seller/1")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Car deleted successfully"}
