f337d63c777e5fa0fa3c057b6b551237057edde0
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-08-27 21:15:26 +0200

    create test for create seller

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,24 +1,24 @@
 from .test_main import client
 
-# def test_user_registration():
-#     response = client.post(
-#         "/auth/register/",
-#         json={
-#             "username": "test",
-#             "fistname": "testfirstname",
-#             "lastname": "testlastname",
-#             "email": "test@test.com",
-#             "password": "bUeN0"
-#         }
-#     )
-#     assert response.status_code == 201
-#     assert response.json() == {
-#         "username": "test",
-#         "fistname": "testfirstname",
-#         "lastname": "testlastname",
-#         "email": "test@test.com",
-#         "password": "bUeN0"
-#     }
+def test_user_registration():
+    response = client.post(
+        "/auth/register/",
+        json={
+            "username": "test",
+            "fistname": "testfirstname",
+            "lastname": "testlastname",
+            "email": "test@test.com",
+            "password": "bUeN0"
+        }
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        "username": "test",
+        "fistname": "testfirstname",
+        "lastname": "testlastname",
+        "email": "test@test.com",
+        "password": "bUeN0"
+    }
 
 def test_user_login():
     response = client.post(
