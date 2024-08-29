f19cc00dc2fb82f7fdcdb8aad08ddd5961a99e8b
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-08-29 22:12:39 +0200

    add data to DB

--- backend/create_data.py
+++ backend/create_data.py
@@ -3,13 +3,13 @@ from database.models import *
 from main import db_dependency
 
 def create_data(db: db_dependency):
-    db_brand = Brand(name="Mercedes", id=2)
-    db_model = Model(name="Class G", id=4, brand_id=2, type_id=2)
-    db_type = Type(name="Personal", id=2)
-    db_fuel = Fuel(name="Diesel", id=2)
-    db_car_body = CarBody(name="SUV", id=2)
-    db_gearbox = Gearbox(name="Automatic", id=2)
-    db_country = Country(name="Germany", id=2)
+    db_brand = Brand(name="Mercedes", id=1)
+    db_model = Model(name="Class G", id=1, brand_id=1, type_id=1)
+    db_type = Type(name="Personal", id=1)
+    db_fuel = Fuel(name="Diesel", id=1)
+    db_car_body = CarBody(name="SUV", id=1)
+    db_gearbox = Gearbox(name="Automatic", id=1)
+    db_country = Country(name="Germany", id=1)
     db.add_all([db_brand, db_model, db_type, db_fuel, db_car_body, db_gearbox, db_country])
     db.commit()
 
