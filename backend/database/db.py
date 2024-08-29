8d9ffad76cc08e2a96b4f4f0c7944af8634b116d
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-08-29 17:14:01 +0200

    function for data

--- backend/database/db.py
+++ backend/database/db.py
@@ -8,6 +8,6 @@ load_dotenv()
 
 DB_URL = "postgresql://root:bUeN0@localhost:5432/cardb"
 
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
