35bb09021ddca81c88805aa4e89f9dc7a238587a
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-09-01 23:51:57 +0200

    connecting fastapi and react

--- backend/tests/test_3_main.py
+++ backend/tests/test_3_main.py
@@ -7,7 +7,25 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    # assert response.json() == datas
+    assert response.json() == {
+        "id": 1,
+        "name": "hi",
+        "brand": 1,
+        "model": 1,
+        "type": 1,
+        "fuel": 1,
+        "tachometer": 50,
+        "made_at": 2022,
+        "description": "hi",
+        "car_body": 1,
+        "gearbox": 1,
+        "power": 588,
+        "place_of_sale": "hi",
+        "country_of_car": 1,
+        "history": "hi",
+        "price": 9700000,
+        "min_price": 9500000
+    }
 
 def test_delete_seller():
     response = client.delete(
