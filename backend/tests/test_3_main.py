21d3c54ddcf9b8f52fc93bfa91d29d040501ada1
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-09-02 22:45:28 +0200

    return data

--- backend/tests/test_3_main.py
+++ backend/tests/test_3_main.py
@@ -8,28 +8,27 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == [{
-        "id": 1,
-        "user": {
-            "name": "test",
-            "email": "test@test.com"
-        },
-        "price": 9700000,
-        "min_price": 9500000,
-        "sold": False,
-        "car": {
-            "brand": "Mercedes",
-            "model": "Class G",
-            "type": "Personal",
-            "fuel": "Diesel",
-            "tachometer": 50,
-            "made_at": 2022,
-            "description": "hi",
-            "car_body": "SUV",
-            "gearbox": "Automatic",
-            "power": 588,
-            "place_of_sale": "hi",
-            "country": "Germany",
-            "history": "hi",
+        'id': 1, 
+        'user': {
+            'name': 'test', 
+            'email': 'test@test.com'
+        }, 
+        'price': 9700000, 
+        'min_price': 9500000, 
+        'sold': False, 
+        'car': {
+            'brand': 'Mercedes', 
+            'model': 'Class G', 
+            'type': 'Personal', 
+            'fuel': 'Diesel', 
+            'tachometer': 50, 
+            'made_at': 2022, 
+            'car_body': 'SUV', 
+            'gearbox': 'Automatic', 
+            'power': 588, 
+            'place_of_sale': 'hi', 
+            'country': 'Germany', 
+            'history': 'hi'
         }
     }]
 
