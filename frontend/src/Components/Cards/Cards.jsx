7583bbeb261d2fadcc51cac2b82fe73f32753b6d
amir <amir.abdullaev@email.cz>
2024-11-21 23:30:19 +0100

    redesigning the style of home page

---
+++ frontend/src/Components/Cards/Cards.jsx
@@ -0,0 +1,127 @@
+import { Button } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useNavigate } from 'react-router-dom'
+import Modals from '../BuyerForm/Modals.jsx'
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBBtn
+  } from 'mdb-react-ui-kit';  
+
+function Cards(props) {
+    const navigate = useNavigate();
+    // const id = props.id
+    const handleClink = () => {
+        navigate(`/seller/${props.id}`)
+    }
+    
+    const isSaved = (
+        <Button variant="primary" className="mx-3" style={{width: "7rem"}}><i class="bi bi-bookmark-fill"></i></Button>
+    )
+    const isNotSaved = (
+        <Button variant="outline-primary" className="mx-3" style={{width: "7rem"}}><i class="bi bi-bookmark"></i></Button>
+    )
+
+    return (
+        <>
+        {/* <Card  
+            // style={{ 
+            //     width: '100%',
+            //     height: '100%',
+            //     borderRadius: '15px',
+            //     transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+            //     overflow: 'hidden'
+            // }}
+            onMouseEnter={(e) => {
+                e.currentTarget.style.transform = "scale(1.02)";
+                e.currentTarget.style.boxShadow = "0 4px 20px rgba(0,0,0,0.1"
+            }}
+            onMouseLeave={(e) => {
+                e.currentTarget.style.transform = "scale(1)";
+                e.currentTarget.style.boxShadow = "none"
+            }}
+            className="mb-3 p-2"
+            key={props.id}
+        >
+            <Row className="no-gutters">
+                <Col md={4}>
+                    <Card.Img variant="top" src={image} alt="img" style={{height: '100%', width: '100%', objectFit: 'cover'}}/>
+                </Col>
+                <Col md={8}>
+                    <Card.Body>
+                        <Card.Title style={{
+                            fontSize: '1.5rem', 
+                            fontWeight: 'bold',
+                        }}
+                        className="cartitle" 
+                        onClick={() => handleClink()}>
+                            {props.brand} {props.model} 
+                        </Card.Title>
+                        <Card.Text>
+                            <i class="bi bi-calendar"> {props.made_at},</i> 
+                            <i class="bi bi-speedometer2 mx-2"> {props.tachometer}km,</i>  
+                            <i class="bi bi-gear"> {props.power}kW,</i>
+                            <i class="bi bi-geo-alt mx-2"> {props.place_of_sale},</i>  
+                            <i class="bi bi-fuel-pump"> {props.fuel},</i><br />
+                            <i class="bi bi-joystick"> {props.gearbox}</i>
+                        </Card.Text>
+                        <Card.Text>
+                            <span className="badge bg-secondary">Price: {props.price}</span>
+                            <span className="badge bg-secondary m-1">Min Price: {props.min_price}</span>
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Modals brand={props.brand} model={props.model} price={props.price} />
+                        {props.saved ? isSaved : isNotSaved}
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Card> */}
+        <MDBCard
+            onMouseEnter={(e) => {
+                e.currentTarget.style.transform = "scale(1.02)";
+                e.currentTarget.style.boxShadow = "0 4px 20px rgba(0,0,0,0.1"
+            }}
+            onMouseLeave={(e) => {
+                e.currentTarget.style.transform = "scale(1)";
+                e.currentTarget.style.boxShadow = "none"
+            }}
+        >
+            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+            <MDBCardBody>
+                <MDBCardTitle 
+                    onClick={() => handleClink()}
+                    style={{
+                        fontSize: '1.5rem', 
+                        fontWeight: 'bold',
+                    }}
+                    className="cartitle" 
+                >
+                    {props.brand} {props.model} 
+                </MDBCardTitle>
+                <MDBCardText>
+                    <i className="bi bi-calendar"> {props.made_at},</i> 
+                    <i className="bi bi-speedometer2 mx-2"> {props.tachometer}km,</i>  
+                    <i className="bi bi-gear"> {props.power}kW,</i>
+                    <i className="bi bi-geo-alt mx-2"> {props.place_of_sale},</i>  
+                    <i className="bi bi-fuel-pump"> {props.fuel},</i><br />
+                    <i className="bi bi-joystick"> {props.gearbox}</i>
+                </MDBCardText>
+                <MDBCardText>
+                    <span className="badge bg-secondary">Price: {props.price}</span>
+                    <span className="badge bg-secondary m-1">Min Price: {props.min_price}</span>
+                </MDBCardText>
+            </MDBCardBody>
+            <MDBCardBody>
+                <Modals brand={props.brand} model={props.model} price={props.price} />
+                {props.saved ? isSaved : isNotSaved}
+            </MDBCardBody>
+        </MDBCard>
+        </>
+    )
+}
+
+export default Cards
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,15 +1,20 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
-import Cards from '../Components/Cards.jsx';
+// import Cards from '../Components/Cards.jsx';
 import Filters from "../Components/Filters/Filters.jsx";
 import React, { useState, useEffect } from "react";
 import CardLoading from "../Components/Loading/CardLoading.jsx";
 import './Home.css'
+import Cards from '../Components/Cards/Cards.jsx';
 
-// const car = [
-//     { id: 1, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: './image/bmwi81.jpg' },
-//     { id: 2, title: 'Mercedes-Benz S class', text: 'My second favorite car', price: '$250000', image: 'bmwi81'},
-//     { id: 3, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: 'bmwi81' },
-// ];
+const car = [
+    { id: 1, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: './image/bmwi81.jpg' },
+    { id: 2, title: 'Mercedes-Benz S class', text: 'My second favorite car', price: '$250000', image: 'bmwi81'},
+    { id: 3, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: 'bmwi81' },
+    { id: 4, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: 'bmwi81' },
+    { id: 5, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: 'bmwi81' },
+    { id: 6, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: 'bmwi81' },
+    { id: 7, title: 'BMW I8', text: 'My favorite car', price: '$250000', image: 'bmwi81' },
+];
 
 function Home() {
     const [data, setData] = useState(null);
@@ -36,20 +41,39 @@ function Home() {
 
     if (loading) return <CardLoading />;
     if (error) return <p>Error: {error.message}</p>; 
-    const cards = data.map((cars) => (
-        <Col xs={12} key={cars.id} className="mb-4">
+    // const cards = data.map((cars) => (
+    //     <Col xs={12} key={cars.id} className="mb-4">
+    //         <Cards 
+    //             id={cars.id} 
+    //             brand={cars.car.brand} 
+    //             model={cars.car.model} 
+    //             price={cars.price} 
+    //             min_price={cars.min_price} 
+    //             made_at={cars.car.made_at} 
+    //             tachometer={cars.car.tachometer} 
+    //             power={cars.car.power} 
+    //             fuel={cars.car.fuel} 
+    //             gearbox={cars.car.gearbox} 
+    //             place_of_sale={cars.car.place_of_sale} 
+    //             saved={true}
+    //         />
+    //     </Col>
+    // ));
+
+    const cards = car.map((cars) => (
+        <Col key={cars.id} className="col-md-6 mb-4">
             <Cards 
                 id={cars.id} 
-                brand={cars.car.brand} 
-                model={cars.car.model} 
+                brand={cars.title} 
+                model={cars.text} 
                 price={cars.price} 
-                min_price={cars.min_price} 
-                made_at={cars.car.made_at} 
-                tachometer={cars.car.tachometer} 
-                power={cars.car.power} 
-                fuel={cars.car.fuel} 
-                gearbox={cars.car.gearbox} 
-                place_of_sale={cars.car.place_of_sale} 
+                min_price={cars.price} 
+                made_at={cars.title} 
+                tachometer={cars.title} 
+                power={cars.title} 
+                fuel={cars.title} 
+                gearbox={cars.title} 
+                place_of_sale={cars.title} 
                 saved={true}
             />
         </Col>
@@ -58,10 +82,10 @@ function Home() {
     return (
         <Container className="mt-5">
             <Row>
-                <Col xs={12} md={4} className="mb-4 my-4">
+                <Col xs={12} md={3} className="mb-4 my-4">
                     <Filters />
                 </Col>
-                <Col xs={12} md={8}>
+                <Col className="mb-4 my-4" xs={12} md={8}>
                     <Row>
                         {cards}
                     </Row>
